5480aed638f69dbe5c49a97c18a595f9d21ece8d
RIchard O'Beirne <rjob.obeirne@gmail.com>
2021-08-07 11:28:04 +1000

    Additional name change

--- app/src/androidTest/java/com/sail/sailright/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sail/sailright/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.sail.sailright2", appContext.getPackageName());
+        assertEquals("com.sail.sailright", appContext.getPackageName());
     }
 }
