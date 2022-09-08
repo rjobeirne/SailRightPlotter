da33290b18cc5b0c16a6787e92487ce3bd21eaf0
RIchard O'Beirne <rjob.obeirne@gmail.com>
2022-09-08 14:53:46 +1000

    Correct Start hide nav bar to sticky

--- app/src/main/java/com/sail/sailright/StartActivity.java
+++ app/src/main/java/com/sail/sailright/StartActivity.java
@@ -423,7 +423,7 @@ public class StartActivity extends AppCompatActivity {
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY70 ;/
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
