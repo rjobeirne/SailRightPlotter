9401dfda31d6e497b4d182cbcb6e55ed8cfbff68
RIchard O'Beirne <rjob.obeirne@gmail.com>
2025-01-13 15:28:20 +1100

    Corrected extent of Test mode

--- app/src/main/java/com/sail/sailrightplotter/MainActivity.java
+++ app/src/main/java/com/sail/sailrightplotter/MainActivity.java
@@ -299,10 +299,10 @@ public class MainActivity extends AppCompatActivity {
         // Set test mode map limits
         pNorth = -37.82; // production mode
         pEast = 145.05;  // production mode
-        Boolean shMode = TRUE;
+        Boolean shMode = FALSE;
         if (shMode)  {
             pNorth = -37.62;
-            pEast = 145.25;
+            pEast = 145.11;
         }
 
         // Go to settings page on long click
